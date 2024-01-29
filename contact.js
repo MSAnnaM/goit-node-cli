567f803d55658824a752723c07f6b40102fbf93c
MSAnnaM <133233271+MSAnnaM@users.noreply.github.com>
2024-01-29 20:37:50 +0200

    final add

---
+++ contact.js
@@ -0,0 +1,68 @@
+import fs from "fs/promises";
+import path from "path";
+
+const pathToFile = path.join("db", "contacts.json");
+
+
+export async function listContacts() {
+  try {
+    const data = await fs.readFile(pathToFile, "utf-8");
+    const contactsList = JSON.parse(data);
+    return contactsList;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+export async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(pathToFile, "utf-8");
+    const contacts = JSON.parse(data);
+    const contactById = contacts.find((contact) => contact.id === contactId);
+    return contactById || null;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+export async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(pathToFile, "utf-8");
+    const contacts = JSON.parse(data);
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    if (index !== -1) {
+      const remove = contacts.splice(index, 1)[0];
+      await fs.writeFile(
+        pathToFile,
+        JSON.stringify(contacts, null, 2),
+        "utf-8"
+      );
+      return remove;
+    }
+    return null;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+export async function addContact(name, email, phone) {
+  try {
+    const data = await fs.readFile(pathToFile, "utf-8");
+    const contacts = JSON.parse(data);
+    const newContact = {
+      id: new Date().getTime().toString(16),
+      name,
+      email,
+      phone,
+      };
+      contacts.push(newContact);
+      await fs.writeFile(
+        pathToFile,
+        JSON.stringify(contacts, null, 2),
+        "utf-8"
+      );
+      return newContact;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
---
+++ index.js
@@ -0,0 +1,38 @@
+import { program } from "commander";
+import { listContacts, getContactById, removeContact, addContact } from "./contact.js";
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+// TODO: рефакторити
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+          listContacts().then(console.table);
+      break;
+
+    case "get":
+          getContactById(id).then((contact) => console.log(contact || "There isn't contact with this id"));
+      break;
+
+    case "add":
+          addContact(name, email, phone).then((contact) => console.log(contact));
+      break;
+
+    case "remove":
+      removeContact(id).then(contact=>console.log(contact))
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
